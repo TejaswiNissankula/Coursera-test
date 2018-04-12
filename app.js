6363ab5d0f66c44b17167c09b48a813515b652d9
TejaswiNissankula <38242462+TejaswiNissankula@users.noreply.github.com>
2018-04-12 16:24:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 (function (){
-//'use strict';
+'use strict';
 
   angular.module('myFirstAngularApp',[])
 
-  .controller('myFirstController',function ($scope){
+  .controller('myFirstController',['$scope',function ($scope){
     $scope.name="Tejaswi";
     $scope.car="JAZZ";
-  });
+  }]);
 })();
