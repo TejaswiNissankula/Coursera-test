b1ef01a2c547f7d36571572602c0750e7b747769
Vinay Tej Nissankula <vinaytejnissankula@Vinays-MacBook-Pro.local>
2018-05-16 10:48:14 +0530

    c6

--- Mod1_Solution/app.js
+++ Mod1_Solution/app.js
@@ -13,15 +13,15 @@
 
       var itemList=$scope.name.split(',');
 
-        if(itemList.size()<=3){
+        if(itemList.size<=3){
 
           $scope.displayMessage="Enjoy!";
 
-        }else if(itemList.size()>3){
+        }else if(itemList.size>3){
 
           $scope.displayMessage="Too much!";
 
-        }else if(itemList.size()==0){
+        }else if(itemList.size==0){
 
           $scope.displayMessage="Please enter data first";
 
